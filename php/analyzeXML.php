181c4910a83186ba6a6e1de45a5cc164718ef78d
donkilu <b99901104@ntu.edu.tw>
2015-06-20 16:44:30 +0800

    XML analysis part complete
    Now do the html part and auto download function

---
+++ php/analyzeXML.php
@@ -0,0 +1,43 @@
+<?php
+	// read target station from AJAX POST
+	if(!$_POST['target']) die("0");
+	$target = $_POST['target'];
+	// read xml file
+	date_default_timezone_set("Asia/Taipei");
+	$time_now = date('H:i:s'); 
+	$xml_dir  = "../XML/" . date(Ymd) . ".xml";
+	$xml = simplexml_load_file($xml_dir) or die("Error: Cannot create object");
+	// start to search available train schedules
+	$schedule_array = array();
+	foreach($xml->children() as $traininfo){
+		// the simplexml method returns xml object, need (string) to transform into string.
+		$schedule = array(
+			'Train'    => (string)$traininfo['Train'],
+			'CarClass' => (string)$traininfo['CarClass'],
+			'DEPTime'  => '',
+			'Dest'     => '',
+			'flag'     => false
+		);
+		foreach($traininfo->children() as $timeinfo){
+			// if the train stops at the target station
+			if($timeinfo['Station'] == $target){
+				// if the train departure time is after present time
+				if( date('H:i:s', strtotime($timeinfo['DEPTime'])) > date('H:i:s') ){
+					$schedule['flag'] = true;
+					$schedule['DEPTime'] = (string)$timeinfo['DEPTime'];
+				}
+			}	
+			$schedule['Dest'] = (string)$timeinfo['Station'];
+		}
+		if($schedule['flag'])  array_push($schedule_array, $schedule);
+	}
+	// sorting function (brilliant stackoverflow!)
+	function sortByOrder($a, $b) {
+		return $a['DEPTime'] - $b['DEPTime'];
+	}
+	usort($schedule_array, 'sortByOrder');
+	// we only need last five schedules
+	$Last_five_array = array_slice($schedule_array, 0, 5);
+	header('Content-Type: application/json');
+	echo json_encode($Last_five_array);
+?>
--- php/downloadXML.php
+++ php/downloadXML.php
@@ -1,53 +1,67 @@
 <html>
 <?php
-	// load php zip extension if not loaded
-	if (!extension_loaded('zip')) {  
-		dl('zip.so');  
+	// first check whether today's xml is downloaded
+	date_default_timezone_set("Asia/Taipei");
+	$today_str = date(Ymd);
+	$xml_dir   = "../XML/";
+	$local_xml = $xml_dir . $today_str . ".xml";
+	if( file_exists($local_xml) ){
+		echo $today_str . ".xml already downloaded!</br>";
 	}
-	
-	$target_url = "http://163.29.3.98/XML/" + date(Ymd) + ".zip";
-	$userAgent = 'Donki Works (http://donki.comuv.com)';  
-	$file_zip = "../XML/new.zip";  
-	$file_txt = "../XML/";  
-	echo "<br>Starting<br>Target_url: $target_url";  
-	echo "<br>Headers stripped out";  
-	// make the cURL request to $target_url  
-	$ch = curl_init();  
-	$fp = fopen("$file_zip", "w");  
-	curl_setopt($ch, CURLOPT_USERAGENT, $userAgent);  
-	curl_setopt($ch, CURLOPT_URL,$target_url);  
-	curl_setopt($ch, CURLOPT_FAILONERROR, true);  
-	curl_setopt($ch, CURLOPT_HEADER,0);  
-	//curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);  
-	curl_setopt($ch, CURLOPT_AUTOREFERER, true);  
-	curl_setopt($ch, CURLOPT_BINARYTRANSFER,true);  
-	curl_setopt($ch, CURLOPT_TIMEOUT, 10);  
-	curl_setopt ($ch, CURLOPT_SSL_VERIFYHOST, 0);  
-	curl_setopt ($ch, CURLOPT_SSL_VERIFYPEER, 0);   
-	curl_setopt($ch, CURLOPT_FILE, $fp);  
-	$page = curl_exec($ch);  
-	if (!$page) {  
-		echo "<br />cURL error number:" .curl_errno($ch);  
-		echo "<br />cURL error:" . curl_error($ch);  
-		exit;  
-	}  
-	curl_close($ch);  
-	echo "<br>Downloaded file: $target_url";  
-	echo "<br>Saved as file: $file_zip";  
-	echo "<br>About to unzip ...";  
-	// Un zip the file  
-	$zip = new ZipArchive;  
-	if (! $zip) {  
-		echo "<br>Could not make ZipArchive object.";  
-		exit;  
-	}  
-	if($zip->open("$file_zip") != "true") {  
-	   echo "<br>Could not open $file_zip";  
-	}  
-	$zip->extractTo("$file_txt");  
-	$zip->close();  
-	echo "<br>Unzipped file to: $file_txt<br><br>";
-
+	else{
+		// delete all the old files in ../XML/
+		foreach(glob($xml_dir."*.*") as $filename){
+			if(is_file($filename)){
+				unlink($filename);
+			}
+		}		
+		// load php zip extension if not loaded
+		if (!extension_loaded('zip')) {  
+			dl('zip.so');  
+		}
+		$target_url = "http://163.29.3.98/XML/" . $today_str . ".zip";
+		//$target_url = "http://163.29.3.98/XML/20150620.zip";	
+		$userAgent = 'Donki Works (http://donki.comuv.com)';  
+		$file_zip = $xml_dir . $today_str . ".zip";  
+		echo "<br>Starting<br>Target_url: $target_url";  
+		echo "<br>Headers stripped out";  
+		// make the cURL request to $target_url  
+		$ch = curl_init();  
+		$fp = fopen("$file_zip", "w");  
+		curl_setopt($ch, CURLOPT_USERAGENT, $userAgent);  
+		curl_setopt($ch, CURLOPT_URL,$target_url);  
+		curl_setopt($ch, CURLOPT_FAILONERROR, true);  
+		curl_setopt($ch, CURLOPT_HEADER,0);  
+		//curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);  
+		curl_setopt($ch, CURLOPT_AUTOREFERER, true);  
+		curl_setopt($ch, CURLOPT_BINARYTRANSFER,true);  
+		curl_setopt($ch, CURLOPT_TIMEOUT, 10);  
+		curl_setopt ($ch, CURLOPT_SSL_VERIFYHOST, 0);  
+		curl_setopt ($ch, CURLOPT_SSL_VERIFYPEER, 0);   
+		curl_setopt($ch, CURLOPT_FILE, $fp);  
+		$page = curl_exec($ch);  
+		if (!$page) {  
+			echo "<br />cURL error number:" .curl_errno($ch);  
+			echo "<br />cURL error:" . curl_error($ch);  
+			exit;  
+		}  
+		curl_close($ch);  
+		echo "<br>Downloaded file: $target_url";  
+		echo "<br>Saved as file: $file_zip";  
+		echo "<br>About to unzip ...";  
+		// Un zip the file  
+		$zip = new ZipArchive;  
+		if (! $zip) {  
+			echo "<br>Could not make ZipArchive object.";  
+			exit;  
+		}  
+		if($zip->open("$file_zip") != "true") {  
+		   echo "<br>Could not open $file_zip";  
+		}  
+		$zip->extractTo("$xml_dir");  
+		$zip->close();  
+		echo "<br>Unzipped file to: $xml_dir<br><br>";
+	}
 	//$url = "../index.html";
 	//Header("Location:$url");
 ?>
