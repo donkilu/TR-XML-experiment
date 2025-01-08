aab96299e9adab2fa22166cc6fbd79cfef35e804
Eric Lu <ericlu@nvidia.com>
2025-01-07 17:04:11 -0800

    Update analyzer to match ToT TRA XML format

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
-# TRA_realtime_schedule
-A project to analyze Taiwan Railway's XML file and show the results on website!
+# TRA Realtime Schedule
+
+A project to analyze Taiwan Railway's schedule XML file and show the results on website!
 
 Please see http://donkilu.github.io/TRA_realtime_schedule for more information about the project.
-The online demonstration can be found at http://donki.byethost7.com/TR.html
+
+## How to run the app locally
+
+1.  git clone this repo
+
+1.  Install php. For Debian/Ubuntu, you might need to install from ppa:ondrej/php.
+
+1.  Run php server at repo root:
+
+    php -S 127.0.0.1:8000
+
+1.  Open browser and navigate to localhost:8000/TR.html
--- php/analyzeXML.php
+++ php/analyzeXML.php
@@ -1,56 +1,75 @@
 <?php
-	// read target station from AJAX POST
-	if(!$_POST['target']) die("0");
-	$target = $_POST['target'];
-	// read xml file
-	date_default_timezone_set("Asia/Taipei");
-	$time_now = date('H:i:s'); 
-	$xml_dir  = "../XML/" . date("Ymd") . ".xml";
-	if(file_exists($xml_dir)){
-		$xml = simplexml_load_file($xml_dir) or die("Error: Cannot create object");
-	}
-	else{  // if xml file doesn't exist, go to download
-		header("Location:downloadXML.php");
-		exit();
-	}
-	// start to search available train schedules
-	$schedule_array_0 = array();
-	$schedule_array_1 = array();	
-	foreach($xml->children() as $traininfo){
-		// the simplexml method returns xml object, need (string) to transform into string.
-		$schedule = array(
-			'Train'    => (string)$traininfo['Train'],
-			'CarClass' => (string)$traininfo['CarClass'],
-			'DEPTime'  => '',
-			'Dest'     => '',
-			'Dir'      => (string)$traininfo['LineDir'],
-			'flag'     => false
-		);
-		foreach($traininfo->children() as $timeinfo){
-			// if the train stops at the target station
-			if($timeinfo['Station'] == $target){
-				// if the train departure time is after present time
-				if( date('H:i:s', strtotime($timeinfo['DEPTime'])) > date('H:i:s') ){
-					$schedule['flag'] = true;
-					$schedule['DEPTime'] = (string)$timeinfo['DEPTime'];
-				}
-			}	
-			$schedule['Dest'] = (string)$timeinfo['Station'];
-		}
-		// filter out non-stops & terminal stop trains
-		if($schedule['flag'] && $schedule['Dest'] != $target){
-			if($traininfo['LineDir'] == '0') array_push($schedule_array_0, $schedule);
-			else  array_push($schedule_array_1, $schedule);
-		}
-	}
-	// sorting function (brilliant stackoverflow!)
-	function sortByOrder($a, $b) {
-		return strtotime($a['DEPTime']) - strtotime($b['DEPTime']);
-	}
-	usort($schedule_array_0, 'sortByOrder');
-	usort($schedule_array_1, 'sortByOrder');
-	// we only need last six schedules
-	$Last_six_array = array_merge( array_slice($schedule_array_0, 0, 3), array_slice($schedule_array_1, 0, 3));
-	header('Content-Type: application/json');
-	echo json_encode($Last_six_array);
+    // read target station from AJAX POST
+    if(!$_POST['target'])
+    {
+        die("0");
+    }
+
+    $target = $_POST['target'];
+    // read xml file
+    date_default_timezone_set("Asia/Taipei");
+    $time_now = date('H:i:s'); 
+    $xml_dir  = "../XML/" . date("Ymd") . ".xml";
+    if(file_exists($xml_dir))
+    {
+        $xml = simplexml_load_file($xml_dir) or die("Error: Cannot create object");
+    }
+    else
+    {
+        // if xml file doesn't exist, go to download
+        header("Location:downloadXML.php");
+        exit();
+    }
+    // start to search available train schedules
+    $schedule_array_clkwise = array();
+    $schedule_array_counterclk = array();
+    foreach($xml->TrainInfo as $traininfo)
+    {
+        // the simplexml method returns xml object, need (string) to transform into string.
+        $schedule = array(
+            'Train'    => (string)$traininfo['Train'],
+            'CarClass' => (string)$traininfo['CarClass'],
+            'DEPTime'  => '',
+            'Dest'     => '',
+            'Dir'      => (string)$traininfo['LineDir'],
+            'flag'     => false
+        );
+
+        foreach($traininfo->children() as $timeinfo){
+            // if the train stops at the target station
+            if($timeinfo['Station'] == $target)
+            {
+                // if the train departure time is after present time
+                if( date('H:i:s', strtotime($timeinfo['DEPTime'])) > date('H:i:s') ){
+                    $schedule['flag'] = true;
+                    $schedule['DEPTime'] = (string)$timeinfo['DEPTime'];
+                }
+            }    
+            $schedule['Dest'] = (string)$timeinfo['Station'];
+        }
+
+        // filter out non-stops & terminal stop trains
+        if($schedule['flag'] && $schedule['Dest'] != $target)
+        {
+            if($traininfo['LineDir'] == '1')
+            {
+                array_push($schedule_array_clkwise, $schedule);
+            }
+            else
+            {
+                array_push($schedule_array_counterclk, $schedule);
+            }
+        }
+    }
+
+    // sorting function (brilliant stackoverflow!)
+    function sortByOrder($a, $b) {
+        return strtotime($a['DEPTime']) - strtotime($b['DEPTime']);
+    }
+    usort($schedule_array_clkwise, 'sortByOrder');
+    usort($schedule_array_counterclk, 'sortByOrder');
+    // we only need last six schedules
+    $last_six_array = array_merge(array_slice($schedule_array_clkwise, 0, 3), array_slice($schedule_array_counterclk, 0, 3));
+    header('Content-Type: application/json');
+    echo json_encode($last_six_array);
 ?>
